13984ac80e91b934e9bc9ce29c1e14caa3981f58
briankipchirchir <kbrianngetich@gmail.com>
2026-02-27 21:35:13 +0300

    kk

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from "react";
-const BASE_URL = "https://captainbackend.onrender.com";
+const BASE_URL = "https://captainbackend-8vob.onrender.com";
 
 type Boost = {
   id: number;
